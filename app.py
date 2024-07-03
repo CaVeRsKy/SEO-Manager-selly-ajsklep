38b1b17b8a64f919f20c8a2d74e28ed709f317ef
CaVeRsKy <wiktor1729@gmail.com>
2024-07-03 19:51:33 +0200

    'zmiana'

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ CORS(app, origins=['https://caversky.github.io'], supports_credentials=True)
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
