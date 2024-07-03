32fe97dbef1e98cc1bd89f4261f15e549a309d3c
CaVeRsKy <wiktor1729@gmail.com>
2024-07-03 18:56:49 +0200

    'zmiana'

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": ["https://caversky.github.io"], "supports_credentials": True}})
